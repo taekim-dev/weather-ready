ef65568d9604619a7f59c14e8b3a57ddbb3b1d73
Tae Kim <taekim.dev@gmail.com>
2023-08-20 11:03:49 +0800

    chore: add skeleton files

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+/**
+ * Component: App
+ * Description: Main container that possibly manages routes or major app layouts.
+ * Responsible: Person A
+ */
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Weather Ready</h1>
+    </div>
+  );
+}
+
+export default App;
