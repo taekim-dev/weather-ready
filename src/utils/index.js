7b1ed0a8f475c00b2318bc3f4acee235efa36c57
Tae Kim <taekim.dev@gmail.com>
2023-09-03 21:52:45 +0800

    new function added

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,9 +8,14 @@ function sampleUtilityFunctionToMerge(){
 
     let conflictVariable = "conflict Resolved";
     let addFastForward2 = "fast forward2";
+
     console.log(conflictVariable);
 
     return null;
 
 
+}
+
+function newFunction(){
+    return null;
 }
